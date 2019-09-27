73a5776b302efde1c1a046a990e4118bfae9e1f1
alifalamiin <yoursweetestn@gmail.com>
2019-09-27 09:45:53 +0700

    commited

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -8,11 +8,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
+    private TextView nama;
+    private TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        nama = findViewById(R.id.input_name);
+        output = findViewById(R.id.text_output);
+    }
+
+    public void submit(View view) {
+        output.setText(nama.getText().toString());
     }
 
 }
